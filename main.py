88b9358f812dfcfb764dc28a731d543bf8129c97
Sabitz <2309106010@student.unmul.ac.id>
2025-06-08 17:15:20 +0800

    update nama button

--- main.py
+++ main.py
@@ -37,36 +37,32 @@ class MainApp:
     
     def setup_ui(self):
         # Load Gambar
-        self.image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
-        self.canvas.create_image(206.0, 132.0, image=self.image_1)
-        self.image_2 = PhotoImage(file=relative_to_assets("image_2.png"))
-        self.canvas.create_image(207.0, 469.0, image=self.image_2)
-        self.image_3 = PhotoImage(file=relative_to_assets("image_3.png"))
-        self.canvas.create_image(206.0, 38.0, image=self.image_3)
-       
-        self.image_4 = PhotoImage(file=relative_to_assets("image_4.png"))
-        self.canvas.create_image(206.0, 187.0, image=self.image_4)
-        self.image_5 = PhotoImage(file=relative_to_assets("image_5.png"))
-        self.canvas.create_image(205.0, 187.0, image=self.image_5)
+        self.image = PhotoImage(file=relative_to_assets("Home.png"))
+        self.canvas.create_image(206.0, 376.0, image=self.image)
 
         # Button Keluar
-        self.button_keluar = PhotoImage(file=relative_to_assets("button_1.png"))
-        Button(image=self.button_keluar, borderwidth=0, command=self.keluar).place(x=17.0, y=28.0, width=16.0, height=22.0)
+        self.button_keluar = PhotoImage(file=relative_to_assets("btnKeluar.png"))
+        Button(image=self.button_keluar, borderwidth=0, command=self.keluar).place(x=225.0, y=591.0, width=150.0, height=120.0)
+
         # Button Tambah Transaksi
-        self.button_tambah = PhotoImage(file=relative_to_assets("button_2.png"))
-        Button(image=self.button_tambah, borderwidth=0, command=self.tambah_transaksi).place(x=37.0, y=286.0, width=150.0, height=120.0)
+        self.btn_tambah = PhotoImage(file=relative_to_assets("btnTambah.png"))
+        Button(image=self.btn_tambah, borderwidth=0, command=self.tambah_transaksi).place(x=37.0, y=286.0, width=150.0, height=120.0)
+
         # Button Lihat Transaksi
-        self.button_lihat = PhotoImage(file=relative_to_assets("button_3.png"))
+        self.button_lihat = PhotoImage(file=relative_to_assets("btnLihat.png"))
         Button(image=self.button_lihat, borderwidth=0, command=self.lihat_transaksi).place(x=225.0, y=286.0, width=150.0, height=120.0)
+
         # Button Edit Transaksi
-        self.button_edit = PhotoImage(file=relative_to_assets("button_4.png"))
+        self.button_edit = PhotoImage(file=relative_to_assets("btnEdit.png"))
         Button(image=self.button_edit, borderwidth=0, command=self.edit_transaksi).place(x=37.0, y=438.0, width=150.0, height=120.0)
+
         # Button Hapus Transaksi
-        self.button_hapus = PhotoImage(file=relative_to_assets("button_5.png"))
+        self.button_hapus = PhotoImage(file=relative_to_assets("btnHapus.png"))
         Button(image=self.button_hapus, borderwidth=0, command=self.hapus_transaksi).place(x=225.0, y=438.0, width=150.0, height=120.0)
+
         # Button Laporan
-        self.button_laporan = PhotoImage(file=relative_to_assets("button_6.png"))
-        Button(image=self.button_laporan, borderwidth=0, command=self.laporan).place(x=37.0, y=590.6666870117188, width=338.0, height=120.0)
+        self.button_laporan = PhotoImage(file=relative_to_assets("btnLaporan.png"))
+        Button(image=self.button_laporan, borderwidth=0, command=self.laporan).place(x=37.0, y=591, width=150.0, height=120.0)
     
     @staticmethod
     def center_window(window, width=412, height=752):
